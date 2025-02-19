3a893d34958cfc36f655a58296b1f06d42677cef
Alejo-P <marcelo.pinzon@epn.edu.ec>
2025-02-18 23:57:38 -0500

    Agregar soporte para columnas responsivas en QRPage y ajustar estilos de cuadrícula en App.css

--- src/pages/QRPage.jsx
+++ src/pages/QRPage.jsx
@@ -34,7 +34,7 @@ const QRPage = () => {
                     loadingQRs ? (
                         <LoadingCard />
                     ) : qrList.length > 0 ? (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
                             {
                                 qrList.map((qr, index) => (
                                     <QRCard key={index} QRName={qr.filename} QRImage={qr.url} />
